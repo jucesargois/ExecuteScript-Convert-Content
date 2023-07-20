6ed53619535c286e78922f4e101e28a4e25fbaa3
jucesargois <jucesargois@gmail.com>
2023-07-19 21:27:52 -0300

    change declaration: let to var and test content

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
-let flowFile = session.get();
+var flowFile = session.get();
 if (flowFile != null) {
-    // let StreamCallback = Java.type("org.apache.nifi.processor.io.StreamCallback");
-    let IOUtils = Java.type("org.apache.commons.io.IOUtils");
-	let StandardCharsets = Java.type("java.nio.charset.StandardCharsets");
-	let InputStreamCallback = Java.type("org.apache.nifi.processor.io.InputStreamCallback");
-	let OutputStreamCallback = Java.type("org.apache.nifi.processor.io.OutputStreamCallback");
-    try {
-        session.read(flowFile, new InputStreamCallback(function (inputStream) {
-            let content = JSON.parse(IOUtils.toString(inputStream, StandardCharsets.UTF_8));
-        }));
-        let example = "test"
+  var StreamCallback = Java.type("org.apache.nifi.processor.io.StreamCallback");
+  var IOUtils = Java.type("org.apache.commons.io.IOUtils");
+  var StandardCharsets = Java.type("java.nio.charset.StandardCharsets");
+  var InputStreamCallback = Java.type("org.apache.nifi.processor.io.InputStreamCallback");
+  var OutputStreamCallback = Java.type("org.apache.nifi.processor.io.OutputStreamCallback");
+  try {
+    session.read(flowFile, new InputStreamCallback(function (inputStream) {
+      content = JSON.parse(IOUtils.toString(inputStream, StandardCharsets.UTF_8));
+    }));
+    var example = "test"
+    content.squadName = "Squad Teste"
+    session.write(flowFile, new OutputStreamCallback(function (outputStream) {
+      outputStream.write(JSON.stringify(content).getBytes(StandardCharsets.UTF_8));
+    }));
 
-        session.write(flowFile, new OutputStreamCallback(function (outputStream) {
-            outputStream.write(JSON.stringify(content).getBytes(StandardCharsets.UTF_8));
-        }));
-
-        session.putAttribute(flowFile, 'Example', example);
-        session.transfer(flowFile, REL_SUCCESS);
-    } catch (e) {
-        log.error('JavaScript ERROR:\n' + e.toString());
-        session.transfer(flowFile, REL_FAILURE)
-    }
+    session.putAttribute(flowFile, 'Example', example);
+    session.transfer(flowFile, REL_SUCCESS);
+  } catch (e) {
+    log.error('JavaScript ERROR:\n' + e.toString());
+    session.transfer(flowFile, REL_FAILURE)
+  }
 }
